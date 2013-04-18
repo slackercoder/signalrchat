c9db48d69beb5392620f945a84bd7d20ce666554
mfolino00@hotmail.com <mfolino00@hotmail.com>
2013-04-18 15:03:18 -0400

    Initial Commit.

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalRChat.Models;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(String name, String message)
+        {
+            Clients.All.broadcastMessage(name, message);
+
+            using (DatabaseEntities db = new DatabaseEntities())
+            {
+                Chat chat = new Chat()
+                {
+                    Name = name,
+                    Message = message
+                };
+
+                db.Chats.Add(chat);
+                db.SaveChanges();
+            }
+        }
+    }
+}
